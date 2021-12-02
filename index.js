c2c007d0d668450487910db37d6e9c0d27e35421
Danielle Bencze <daniellebencze@live.com>
2021-12-01 20:06:28 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+const employee = {
+    name: "Danielle",
+    streetAddress: "123 Happy Street" 
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    return {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    };
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    newEmployee,
+    "Sam",
+    "11 Broadway"
+);
+
+
+employee;
+
+newEmployee;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name, value) {
+    const deletedFromEmployeeClone = {...employee};
+    delete deletedFromEmployeeClone.name;
+    delete deletedFromEmployeeClone.streetAddress;
+    return deletedFromEmployeeClone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee.name;
+        delete employee.streetAddress;
+        return employee;
+}
+
